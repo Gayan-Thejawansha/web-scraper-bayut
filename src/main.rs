f83d445e28e5075c7cf9dd58ca24f43e5726721b
user <gayan@github.com>
2022-10-05 17:14:25 +0400

    HTML content download and parse the document

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use reqwest;
+use scraper;
+use scraper::Html;
+
 fn main() {
-    println!("Hello, world!");
+    const URL:&str = "https://www.bayut.com/for-sale/apartments/dubai/"; //URL to be fetched
+
+    let response = download_content(URL.trim());
+    let document = html_parser(&response);
+    print_type_of(&document);
+    //println!("{:?}", document);
+}
+
+fn download_content(url: &str) -> String{
+    let response = reqwest::blocking::get(url.trim()).unwrap().text().unwrap();
+    return response;
+}
+
+fn html_parser(html_string:&str) -> Html{
+    let html_content = scraper::Html::parse_document(&html_string);
+    return html_content;
+}
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
 }
