493ba6b36db05f70462c1ff32231a36ff8566443
user <gayan@github.com>
2022-10-05 17:53:00 +0400

    Added the second filteration and captured the final data to be analyzed

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use reqwest;
 use scraper;
 use scraper::Html;
 use std::collections::HashMap;
+use serde_json::Value;
 
 fn main() {
     const URL:&str = "https://www.bayut.com/for-sale/apartments/dubai/"; //URL to be fetched
@@ -14,6 +15,20 @@ fn main() {
     let results = html_title_selector (&document, "li.ef447dde");
     println!("{:?}", results.get(&1));
     print_type_of(&results);
+
+    for (key, value) in &results {
+        //println!("{}: {}", key, value);
+        let filterd_document = html_fragment_parser(&value);
+        //let filterd_results = html_title_selector (&filterd_document, "div._4041eb80");
+        let filterd_results = html_title_selector (&filterd_document, r#"script[type="application/ld+json"]"#);
+        for (_key2, value2) in &filterd_results {
+
+            display_the_results(value2, key);
+
+        }
+
+ }
+
 }
 
 fn download_content(url: &str) -> String{
@@ -42,4 +57,15 @@ fn html_title_selector (html_content: &Html, selector_title: &str) -> HashMap<i3
 
 fn update_content_map (map: &mut HashMap<i32, String> , key:i32, value:String){
     let _resp = map.insert(key,value);
+}
+
+fn html_fragment_parser(html_string:&str) -> Html{
+    let html_content = scraper::Html::parse_fragment(&html_string);
+    return html_content;
+}
+
+fn display_the_results(html_string:&str, counter:&i32) {
+    //let mut json_string : JsonValue = html_string.parse()?;
+    let object: Value = serde_json::from_str(html_string).unwrap();
+    println!("{} - {}",counter, object["url"]);
 }
